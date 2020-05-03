a1d5b5b7f7492ef3a998bc597dd4f85180a4a0a0
Andrei Maiboroda <am@homembr.ru>
2020-05-03 19:50:35 +0300

    terraform helm

--- terraform-helm/main.tf
+++ terraform-helm/main.tf
@@ -1,30 +1,80 @@
 provider "helm" {
 }
 
+
+resource "kubernetes_namespace" "namespace-monitoring" {
+  metadata {
+    name = var.monitoring-namespace
+  }
+}
+
+resource "kubernetes_namespace" "namespace-logging" {
+  metadata {
+    name = "logging"
+  }
+}
+
+resource "kubernetes_namespace" "namespace-nginx-ingress" {
+  metadata {
+    name = "nginx-ingress"
+  }
+}
+
+resource "kubernetes_secret" "stackdriver-exporter-sa-json" {
+  
+  metadata {
+    name = "stackdriver-exporter-sa-json"
+    namespace = var.monitoring-namespace
+  }
+
+  data = {
+    "credentials.json" = "${file("~/gcp_keys/docker-270618-owner.json")}"
+  }
+
+  type = "Opaque"
+}
+
 resource "helm_release" "nginx-ingress" {
   name      = "nginx-ingress"
   chart     = "../nginx-ingress"
   namespace = "nginx-ingress"
-  recreate_pods = true
+  # recreate_pods = true
 }
 
 resource "helm_release" "prometheus" {
   name      = "prometheus"
   chart     = "../prometheus"
-  namespace = "monitoring"
-  recreate_pods = true
+  namespace = var.monitoring-namespace
+  # recreate_pods = true
+}
+
+resource "helm_release" "stackdriver-exporter" {
+  name      = "stackdriver-exporter"
+  chart     = "../stackdriver-exporter"
+  namespace = var.monitoring-namespace
+  # recreate_pods = true
+
+  set {
+    name  = "stackdriver.projectId"
+    value = var.project
+      }
+
+  set {
+    name  = "stackdriver.serviceAccountSecret"
+    value = var.stackdriverserviceAccountSecret
+      }
 }
 
 resource "helm_release" "grafana" {
   name      = "grafana"
   chart     = "../grafana"
-  namespace = "monitoring"
-  recreate_pods = true
+  namespace = var.monitoring-namespace
+  # recreate_pods = true
 }
 
 resource "helm_release" "efk" {
   name      = "efk"
   chart     = "../efk"
   namespace = "logging"
-  recreate_pods = true
+  # recreate_pods = true
 }
